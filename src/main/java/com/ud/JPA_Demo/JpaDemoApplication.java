bb07aadd9477ef33e47ddbb57698ae52f5d049a2
Uddhav Gawali <uddhavgawali021@gmail.com>
2025-07-04 00:00:15 +0530

    jpa

---
+++ src/main/java/com/ud/JPA_Demo/JpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ud.JPA_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+
+}
